0d40ea1ec03b5f26226e906ebae71ced786f5c47
vladvln <vladvladx25@gmail.com>
2021-01-26 19:57:41 +0300

    second

--- images.php
+++ images.php
@@ -1,10 +1,13 @@
 <?php
 
-$files = $_FILES;
-$tmp_name = $files['picture']['tmp_name'];
-$file_name = $files['picture']['name'];
-move_uploaded_file($tmp_name, "img/$file_name");
-file_put_contents('imgList.txt', $file_name . PHP_EOL, FILE_APPEND);
+$server = $_SERVER;
+if ($server['REQUEST_METHOD'] === 'POST') {
+    $files = $_FILES;
+    $tmp_name = $files['picture']['tmp_name'];
+    $file_name = $files['picture']['name'];
+    move_uploaded_file($tmp_name, "img/$file_name");
+    file_put_contents('imgList.txt', $file_name . PHP_EOL, FILE_APPEND);
+}
 $imageArr = file('imgList.txt', FILE_SKIP_EMPTY_LINES | FILE_IGNORE_NEW_LINES);
 
 ?>
